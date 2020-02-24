c93a3989c2da494ecfc7d3732cb202e9b7ffdea9
Abdelrahman Abdelhafez <abdelrahman@maged.rocks>
2020-02-24 03:09:44 +0100

    :children_crossing: Randomize lyric every 10 seconds

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { randomElement, randomColor } from './utilities';
 import { lyrics } from './data';
 
 function App() {
-  const { line, song } = randomElement(lyrics);
-  const backgroundColor = randomColor();
+  const [lyric, setLyric] = useState({
+    line: 'Blonded',
+    song: 'Frank Ocean Lyrics',
+  });
 
+  const randomizeLyric = () => {
+    const randomLyric = randomElement(lyrics);
+    setLyric(randomLyric);
+  };
+
+  useEffect(() => {
+    const TEN_SECONDS_IN_MILLIS = 1000 * 10;
+
+    const intervalId = setInterval(randomizeLyric, TEN_SECONDS_IN_MILLIS);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  const { line, song } = lyric;
   const delimitedLine = line.replace(/ /g, ' / ');
 
   return (
-    <div style={{ ...styles.container, backgroundColor }}>
+    <div style={{ ...styles.container, backgroundColor: randomColor() }}>
       <h1 style={styles.line}>“{delimitedLine}”</h1>
       <h2 style={styles.song}>{song}</h2>
     </div>
