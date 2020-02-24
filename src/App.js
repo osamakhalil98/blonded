61075f683b5a91df7f4c6b692f7f93639e2bec0c
Abdelrahman Abdelhafez <abdelrahman@maged.rocks>
2020-02-24 03:01:44 +0100

    :sparkles: Render functional app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
 import React from 'react';
-import { randomElement } from './utilities';
+import { randomElement, randomColor } from './utilities';
 import { lyrics } from './data';
 
 function App() {
-  const randomLyric = randomElement(lyrics);
+  const { line, song } = randomElement(lyrics);
+  const backgroundColor = randomColor();
+
+  const delimitedLine = line.replace(/ /g, ' / ');
 
   return (
-    <div>
-      <h1>{randomLyric.line}</h1>
-      <h2>{randomLyric.song}</h2>
+    <div style={{ ...styles.container, backgroundColor }}>
+      <h1 style={styles.line}>“{delimitedLine}”</h1>
+      <h2 style={styles.song}>{song}</h2>
     </div>
   );
 }
+
+const styles = {
+  container: {
+    height: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'red',
+  },
+  line: {
+    margin: 96,
+    textAlign: 'center',
+    fontFamily: 'blonde',
+    color: 'white',
+    fontSize: 50,
+  },
+  song: {
+    textAlign: 'center',
+    fontFamily: 'monospace',
+    color: 'white',
+    fontSize: 35,
+  },
+};
 
 export default App;
